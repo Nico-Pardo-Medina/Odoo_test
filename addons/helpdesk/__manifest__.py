4cc529d2533bec3eb2f51b0886dcf9d29b7ec429
JC-Pardo-Medina <jcparmed@gmail.com>
2025-02-07 12:13:50 +0100

    test 2

--- addons/helpdesk/__manifest__.py
+++ addons/helpdesk/__manifest__.py
@@ -1,14 +1,17 @@
 {
     'name': 'Helpdesk Ticket',
     'version': '1.0',
-    'summary': 'Modulo de gestión de tickets de soporte',
-    'author': 'Tu Nombre',
-    'category': 'Services',
-    'depends': ['base', 'mail'],
+    'description': 'Modulo de gestión de tickets de soporte',
+    'author': 'Nico Pardo Medina',
+    'category': 'Custom',
+    'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
         'views/helpdesk_ticket_view.xml',
     ],
     'installable': True,
     'application': True,
+    'demo': [
+        'demo/demo_data.xml',
+    ],
 }
