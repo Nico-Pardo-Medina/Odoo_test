383342d496c1b49df146a13a000640505672b5d5
JC-Pardo-Medina <jcparmed@gmail.com>
2025-02-07 14:12:13 +0100

    button modification

--- addons/helpdesk/models/helpdesk_ticket.py
+++ addons/helpdesk/models/helpdesk_ticket.py
@@ -9,10 +9,13 @@ class HelpdeskTicket(models.Model):
         ('new', 'nuevo'),
         ('in_progress', 'en progreso'),
         ('resolved', 'resuelto')
-    ], string='Estado', default='new')
+    ], string='Estado', default='new', readonly=True)
     description = fields.Text(string='Descripción del problema')
     create_date = fields.Datetime(string='Fecha de creación', default=fields.Datetime.now, readonly=True)
     user_id = fields.Many2one('res.users', string='Usuario asignado')
 
     def action_mark_resolved(self):
-        self.state = 'resolved'
+        if self.state == 'new':
+            self.state = 'in_progress'
+        elif self.state == 'in_progress':
+            self.state = 'resolved'
